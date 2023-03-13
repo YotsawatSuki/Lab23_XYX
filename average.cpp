78f96c52ad3f36406768982ce19c77413728b563
Yotsawat Sukinee <yotsawat2557@gmail.com>
2023-03-14 01:01:06 +0700

    finish lab23_command line

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc, char* argv[]){
+    if(argc <= 1){ 
+        cout << "Please input numbers to find average.";
+    }else{
+        int sum;
+        for(int i = 1; i<argc; i++){
+            sum += atoi(argv[i]);
+        }
+        cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1) << endl;
+        cout << "---------------------------------";
+    }
+
+    return 0;
+}
